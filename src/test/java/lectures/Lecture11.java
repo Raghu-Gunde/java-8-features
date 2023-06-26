954ae6fa0bab46fafeb0930ddbefd64850b56547
Raghu-Gunde <raghuvarma336@gmail.com>
2023-06-26 15:12:43 +0530

    Lecture-11 completed

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -11,12 +11,17 @@ public class Lecture11 {
   @Test
   public void joiningStrings() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+    String namesSeparatedByComma ="";
+    for(String name : names){
+      namesSeparatedByComma +=name+", ";
+    }
+    System.out.println(namesSeparatedByComma.substring(0,namesSeparatedByComma.length()-2));
   }
 
   @Test
   public void joiningStringsWithStream() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+    String combinedNames = names.stream().collect(Collectors.joining(",", "-", "|"));
+    System.out.println(combinedNames);
   }
 }
