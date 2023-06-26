4889a171ab4b95dec8efd894c375d23c1021832d
Raghu-Gunde <raghuvarma336@gmail.com>
2023-06-26 14:47:10 +0530

    Lecture-9 completed

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -9,10 +9,14 @@ public class Lecture9 {
 
   @Test
   public void reduce() throws Exception {
+    //reduce function is used to combine all the elements and produces a result
     Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
-
+    int sum = Arrays.stream(integers)
+            .reduce(0, (a, b) -> a + b);
+    System.out.println(sum);
+    Integer sumUsingIntSum = Arrays.stream(integers)
+            .reduce(0, Integer::sum);
+    System.out.println(sumUsingIntSum);
   }
-
-
 }
 
